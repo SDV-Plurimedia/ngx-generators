b067776fe99cd2496252ce99b1349439cdf0fd46
Maël Schweighardt <mael.schweighardt@sdv.fr>
2016-07-20 11:38:55 +0200

    crud

--- generator/generators/ngCrud.js
+++ generator/generators/ngCrud.js
@@ -4,8 +4,15 @@ module.exports = {
 
     generate: function(argv) {
 
-        var createFiles = function(url, model, api_endpoint) {
+        var replaceAll = function(str, find, replace) {
+            // définit comment les balises des templates doivent être récupéré (ex: ~~modelName~~)
+            var baliseTpl = "~~";
+            var find = baliseTpl + find + baliseTpl;
 
+            return str.replace(new RegExp(find, 'g'), replace);
+        }
+
+        var createFiles = function(url, model, api_endpoint) {
             //on mets des valeurs par défaut au besoin
             url = (typeof url !== 'undefined' && url !== "") ? url : "crud/users";
             model = (typeof model !== 'undefined' && model !== "") ? model : "user";
@@ -22,28 +29,111 @@ module.exports = {
             var className = helpers.camelize(filename);
             var modelName = helpers.camelize(model);
 
+            var conf = {
+                'url': url,
+                'api_endpoint': api_endpoint,
+                'absoluteDirname': absoluteDirname,
+                'dirname': dirname,
+                'filename': filename,
+                'className': className,
+                'model': model,
+                'modelName': modelName
+            }
+
             // Pour le composant
             // var componentListContent = ;
 
-            var rawFile = new XMLHttpRequest();
-            rawFile.open("GET", "../templates/ngCrud/component.ts.base", false);
-            rawFile.onreadystatechange = function() {
-                if (rawFile.readyState === 4) {
-                    if (rawFile.status === 200 || rawFile.status == 0) {
-                        var allText = rawFile.responseText;
-                    }
+            const readline = require('readline');
+            const fs = require('fs');
+            const options = {
+                flags: 'r',
+                encoding: null,
+                fd: null,
+                mode: 0o666,
+                autoClose: true
+            };
+
+            const rl = readline.createInterface({
+                input: fs.createReadStream('bin/generator/templates/ngCrud/component.ts.base', options)
+            });
+
+            var read = true;
+            var componentListContent = "tmp1";
+            var componentEditContent = "tmp2";
+            var serviceContent = "tmp3";
+
+            rl.on('line', function(line) {
+                if (line == "~~STOP~~")
+                    read = false;
+                if (read) {
+                    var value = line;
+                    if (value.indexOf("~~") > -1)
+                        for (var key in conf)
+                            value = replaceAll(value, key, conf[key]);
+
+                    componentListContent = componentListContent + value + '\n';
                 }
-            }
-            rawFile.send(null);
+            }).on('close', () => {
+                console.log("result: ", componentListContent);
+                process.exit(0);
+            });
 
-            console.log(allText);
-        };
+            var componentListContent = "tmp1";
+            var componentEditContent = "tmp2";
+            var serviceContent = "tmp3";
 
-        var url = helpers.askDataSync('Quelle url voulez vous pour votre page de CRUD? (exemple: crud/users ) ');
-        var model = helpers.askDataSync('Sur quel modele se base cette page? (exemple: user) ');
-        var api = helpers.askDataSync('A quelle API voulez vous connecter ( exemple: "http://jsonplaceholder.typicode.com/users" ou GLOBAL_CONFIG.api_url+"/user" )? ');
+            var asq = require("async");
+            asq.parallel(
+                [
+                    (cb) => {
+                        helpers.createFileIfNotExist(dirname, filename + ".ts", componentListContent, cb)
+                    },
+                    (cb) => {
+                        helpers.createFileIfNotExist(dirname + "/edit", filename + "-edit.ts", componentEditContent, cb)
+                    },
+                    (cb) => {
+                        helpers.createFileIfNotExist("app/services/data", model + ".ts", serviceContent, cb)
+                    }
+                ],
+                function() {
+                    //aprés avoir tout ecris
+                    //console.log("N'oubliez pas de rajouter la route dans votre fichier app.ts");
+                    //console.log("{path: '"+url+"', name: '"+className+"', component: "+className+"Component }");
+                    helpers.askData('Voulez vous regénérer automatiquement les routes? ( y / n) ', (reponse) => {
+                        if (reponse == 'y') {
+                            var route = require("./route");
+                            route.generate();
+                            helpers.printSeparator();
+                            var filename = url.replace(/\//g, '-');
+                            console.log(`N'oubliez pas de rajouter dans app.ts, la route de modification suivante:
+
+                  {
+                    path: '` + url + `/edit/:id',
+                    name: '` + className + `EditId',
+                    component: ComponentProxyFactory({
+                      path: './dist/components/` + url + `/edit/` + filename + `-edit',
+                      provide: m => m.` + className + `Component
+                    })
+                  },
+
+                   `);
+                        } else {
+                            console.log('Ok, Pas de modification de route...')
+                        }
+                    });
+                });
+
+            return true;
+        }
+
+        // var url = helpers.askDataSync('Quelle url voulez vous pour votre page de CRUD? (exemple: crud/users ) ');
+        // var model = helpers.askDataSync('Sur quel modele se base cette page? (exemple: user) ');
+        // var api = helpers.askDataSync('A quelle API voulez vous connecter ( exemple: "http://jsonplaceholder.typicode.com/users" ou GLOBAL_CONFIG.api_url+"/user" )? ');
+
+        var url = "gestion-test";
+        var model = "model-test";
+        var api = 'osseeef';
+
         createFiles(url, model, api);
-
-
     }
 }
--- helpers.js
+++ helpers.js
@@ -1,122 +1,120 @@
 var fs = require('fs');
 
 module.exports = {
-  camelize: function(str){
-    return str.replace(/(?:^|[-]|[_])(\w)/g, function (a , c) {
-      return c ? c.toUpperCase () : '';
-    });
-  },
+    camelize: function(str) {
+        return str.replace(/(?:^|[-]|[_])(\w)/g, function(a, c) {
+            return c ? c.toUpperCase() : '';
+        });
+    },
 
-  printSeparator: function(){
-    console.log("================================================");
-  },
+    printSeparator: function() {
+        console.log("================================================");
+    },
 
-  askDataSync: function(quest){
-    var readlineSync = require('readline-sync');
+    askDataSync: function(quest) {
+        var readlineSync = require('readline-sync');
 
-    // Wait for user's response.
-    var retour = readlineSync.question(quest);
-    return retour;
-  },
+        // Wait for user's response.
+        var retour = readlineSync.question(quest);
+        return retour;
+    },
 
-  askData: function(question,cb){
-    const readline = require('readline');
+    askData: function(question, cb) {
+        const readline = require('readline');
 
-    const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout
-    });
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
 
-    rl.question(question, function(answer){
-        console.log('   --> ', answer);
-        rl.close();
-        //process.stdin.destroy();
-        return cb(answer);
-    });
-  },
+        rl.question(question, function(answer) {
+            console.log('   --> ', answer);
+            rl.close();
+            //process.stdin.destroy();
+            return cb(answer);
+        });
+    },
 
-  getFile(file,cb){
-    fs.readFile(file, (err, data) => {
-      if (err) throw err;
-      cb(data);
-    });
-  },
+    getFile(file, cb) {
+        fs.readFile(file, (err, data) => {
+            if (err) throw err;
+            cb(data);
+        });
+    },
 
-  copyFile(source,destination){
-    fs.createReadStream(source).pipe(fs.createWriteStream(destination));
-  },
+    copyFile(source, destination) {
+        fs.createReadStream(source).pipe(fs.createWriteStream(destination));
+    },
 
-  //remplace debut(.*)fin par donnee dans input
-  replacePlaceHolder(input,debut,fin,donnee){
-      var reg_term = debut+"([^]*)"+fin;
-      var regex = new RegExp(reg_term,"mgi");
-      var retour = (input+"").replace(regex,donnee);
-      return retour;
-  },
+    //remplace debut(.*)fin par donnee dans input
+    replacePlaceHolder(input, debut, fin, donnee) {
+        var reg_term = debut + "([^]*)" + fin;
+        var regex = new RegExp(reg_term, "mgi");
+        var retour = (input + "").replace(regex, donnee);
+        return retour;
+    },
 
-  //remplis debut(.*)fin avec donnee dans input
-  placeInPlaceHolder(input,debut,fin,donnee){
-    return this.replacePlaceHolder(input,debut,fin,debut+donnee+fin);
-  },
+    //remplis debut(.*)fin avec donnee dans input
+    placeInPlaceHolder(input, debut, fin, donnee) {
+        return this.replacePlaceHolder(input, debut, fin, debut + donnee + fin);
+    },
 
-  getFileInDirRecursive: function(folder){
-    var fileContents = fs.readdirSync(folder),
-        filesTab = [],
-        stats;
+    getFileInDirRecursive: function(folder) {
+        var fileContents = fs.readdirSync(folder),
+            filesTab = [],
+            stats;
 
-    fileContents.forEach((fileName)=>{
-        stats = fs.lstatSync(folder + '/' + fileName);
+        fileContents.forEach((fileName) => {
+            stats = fs.lstatSync(folder + '/' + fileName);
 
-        if (stats.isDirectory()) {
-          filesTab = filesTab.concat(this.getFileInDirRecursive(folder + '/' + fileName) );
-        } else {
-          filesTab.push(folder + '/' + fileName);
-        }
-    });
-
-    return filesTab;
-  },
-
-  getListDir: function(path, cb){
-    fs.readdir(path, function(err, items) {
-      cb(items);
-    });
-  },
-
-  //cree un fichier meme s'il existe deja
-  createFile(dirname, filename, content, cb){
-    var mkdirp = require('mkdirp');
-    //var asq = require("async");
-
-    mkdirp(dirname, function (err) {
-        if (err){
-          //console.error(err);
-          if(typeof cb == 'function'){
-            return cb(err);
-          }
-        }
-        else{
-
-          fs.writeFile(dirname+'/'+filename, content, function(err) {
-              if(typeof cb == 'function'){
-                return cb(err);
-              }
-          });
-        }
-    });
-  },
-
-  createFileIfNotExist: function(dirname, filename, content, cb){
-    fs.stat(dirname+'/'+filename, (err, stat) => {
-        if(err !== null) {
-          this.createFile(dirname, filename, content, cb);
-        } else {
-            console.log('Le fichier "'+dirname+'/'+filename+'" existe deja ');
-            if(typeof cb == 'function'){
-              return cb(err);
+            if (stats.isDirectory()) {
+                filesTab = filesTab.concat(this.getFileInDirRecursive(folder + '/' + fileName));
+            } else {
+                filesTab.push(folder + '/' + fileName);
             }
-        }
-    });
-  }
+        });
+
+        return filesTab;
+    },
+
+    getListDir: function(path, cb) {
+        fs.readdir(path, function(err, items) {
+            cb(items);
+        });
+    },
+
+    //cree un fichier meme s'il existe deja
+    createFile(dirname, filename, content, cb) {
+        var mkdirp = require('mkdirp');
+        //var asq = require("async");
+
+        mkdirp(dirname, function(err) {
+            if (err) {
+                console.error("err: ", err);
+                if (typeof cb == 'function') {
+                    return cb(err);
+                }
+            } else {
+                fs.writeFile(dirname + '/' + filename, content, function(err) {
+                    if (typeof cb == 'function') {
+                        return cb(err);
+                    }
+                });
+            }
+        });
+    },
+
+    createFileIfNotExist: function(dirname, filename, content, cb) {
+        fs.stat(dirname + '/' + filename, (err, stat) => {
+            if (err !== null) {
+                this.createFile(dirname, filename, content, cb);
+            } else {
+                console.log('Le fichier "' + dirname + '/' + filename + '" existe deja ');
+                if (typeof cb == 'function') {
+                    return cb(err);
+                }
+            }
+        });
+    }
 
 }
