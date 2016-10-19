e44296b8046cc346ee6b74399a7f7d654b899293
WEBER Antoine <a.weber@sdv.fr>
2016-10-19 15:10:34 +0200

    nettoyage des scripts inutilisés

---
+++ README.md
@@ -0,0 +1,3 @@
+#ng2-generators
+
+s'appelle avec ng2gen dans un script de package.json
--- compil.bash
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run -it --rm --name mon-script -v "$PWD":/usr/src/app -w /usr/src/app node:latest npm run dumpdev
