8ea01cd8f50d5b0d31e6a874586c99974405c302
Antoine WEBER <a.weber@sdv.fr>
2017-06-20 17:03:21 +0200

    ajout d'un générateur de module simple

--- generator/generators/route.js
+++ generator/generators/route.js
@@ -6,45 +6,61 @@ module.exports = {
 
     console.log('Génération automatique des routes de module');
 
-    var route = "";
-    var imports = "";
     var modules = "";
     var importmodules = "";
     var basepath = "src/app/_modules";
 
-    // préparation des includes et des routes
+    // préparation des includes du module
     var files = helpers.getFileInDirRecursive(basepath);
     files.forEach((file)=>{
-
-      var filename = file.split('/').pop();//juste le nom du fichier
       if(
-        file.indexOf('routing.ts') !== -1
-        && file.indexOf('dist') == -1
-        && file.indexOf('modules.module') == -1
-        && file.indexOf('modules.routing') == -1
+        //on recherche les fichiers de module
+        file.indexOf('module.ts') !== -1
+        //exclusions
+        && file.indexOf('dist') === -1
+        && file.indexOf('modules.module') === -1
       ){
-          var path = file.replace(basepath,"").replace("/"+filename,"");
-          var componentpath = file.replace(basepath+"/","./").replace(".ts","");
-          var className = helpers.camelize(filename.replace(".routing.ts",""));
-          var routeName = helpers.lcfirst(className) + "Routes";
+          var filename = file.split('/').pop();//juste le nom du fichier
+          var module_path = file.replace(basepath,'.').replace(".ts","");
+          filename = filename.replace('.module.ts','').replace("mod_",'').replace("_spa",'');
+          var className = helpers.camelize(filename);
 
-          route += `...`+routeName+`,
-`;
-          imports += `import { `+routeName+` } from "`+componentpath+`";
-`;
           modules += className+`Module,
 `;
-          importmodules += "import {"+className+"Module} from '"+file.replace("src/app/_modules",'.').replace("routing.ts","module")+`';
+          importmodules += "import {"+className+"Module} from '"+module_path+`';
 `;
 
         }
     });
 
-    // préparation des widgets
-    var env = [];
-    var env_import = "";
+    var route = "";
+    var imports = "";
+    // préparation des routes
+    files.forEach((file)=>{
+      if(
+        //on recherche les fichiers de module
+        file.indexOf('routing.ts') !== -1
+        //exclusions
+        && file.indexOf('dist') === -1
+        && file.indexOf('modules.routing') === -1
+      ){
+          var filename = file.split('/').pop();//juste le nom du fichier
+          var route_path = file.replace(basepath,'.').replace(".ts","");
 
-    var files = helpers.getFileInDirRecursive(basepath);
+          filename = filename.replace('.routing.ts','').replace("mod_",'').replace("_spa",'');
+          var className = helpers.camelize(filename);
+          var routeName = helpers.lcfirst(className) + "Routes";
+
+          route += `...`+routeName+`,
+`;
+          imports += `import { `+routeName+` } from '`+route_path+`';
+`;
+        }
+    });
+
+    // préparation des widgets
+    var widgets = [];
+    var import_widgets = "";
     files.forEach((file)=>{
 
       var filename = file.split('/').pop();//juste le nom du fichier
@@ -53,35 +69,34 @@ module.exports = {
         && file.indexOf('dist') == -1
         && file.indexOf('modules.widgets') == -1
       ){
-          var path = file.replace(basepath,"").replace("/"+filename,"");
-          var componentpath = file.replace(basepath+"/","./").replace(".ts","");
-          var baseName = helpers.camelize(filename.replace(".widgets.ts",""));
-          var envName = helpers.lcfirst(baseName) + "Widgets";
+          var widget_path = file.replace(basepath+"/","./").replace(".ts","");
+          var className = helpers.camelize(filename.replace(".widgets.ts",""));
+          var widgetName = helpers.lcfirst(className) + "Widgets";
 
-            //on crée l'import des env
-            env_import += `import { widgets as `+envName+` } from '`+componentpath+`';
+            //on crée l'import des widgets
+            import_widgets += `import { widgets as `+widgetName+` } from '`+widget_path+`';
 `;
-            // on stoque un tableau des fichiers env
-            env.push(envName);
+            // on stoque un tableau des fichiers widgets
+            widgets.push(widgetName);
         }
     });
 
-    // génération des merges de tableau pour chaque fichier env
+    // génération des merges de tableau pour chaque fichier widgets
     var art_merge = "";
     var comp_merge = "";
     var comp_popup_merge = "";
     var topic_merge = "";
     var all_meta_merge = "";
-    env.forEach(function(nom_env){
-      art_merge += `...`+nom_env+`.widgets_article_edition,
+    widgets.forEach(function(nom_widgets){
+      art_merge += `...`+nom_widgets+`.widgets_article_edition,
 `;
-      comp_merge += `...`+nom_env+`.widgets_complement_panel,
+      comp_merge += `...`+nom_widgets+`.widgets_complement_panel,
 `;
-      comp_popup_merge += `...`+nom_env+`.widgets_complement_popup,
+      comp_popup_merge += `...`+nom_widgets+`.widgets_complement_popup,
 `;
-      topic_merge += `...`+nom_env+`.widgets_topic_edition,
+      topic_merge += `...`+nom_widgets+`.widgets_topic_edition,
 `;
-      all_meta_merge = "..."+nom_env+`.widgets_all_meta,
+      all_meta_merge = "..."+nom_widgets+`.widgets_all_meta,
 `;
     });
 
@@ -132,14 +147,14 @@ module.exports = {
       });
     });
 
-    // création du fichier env si inexistant
-    var env_file = "modules.widgets.ts";
-    helpers.getFile(template_modules_dir+env_file,(data)=>{
-      helpers.createFileIfNotExist(modules_base_dir, env_file, data, (cb)=>{
-        //maj du fichier d'env global
-        helpers.getFile(modules_base_dir+env_file,(data)=>{
-          env_import = "\n"+env_import+"\n";
-          var retour = helpers.placeInPlaceHolder(data,"// DEBUT-IMPORT-PLACEHOLDER","// FIN-IMPORT-PLACEHOLDER",env_import);
+    // création du fichier widgets si inexistant
+    var widgets_file = "modules.widgets.ts";
+    helpers.getFile(template_modules_dir+widgets_file,(data)=>{
+      helpers.createFileIfNotExist(modules_base_dir, widgets_file, data, (cb)=>{
+        //maj du fichier d'widgets global
+        helpers.getFile(modules_base_dir+widgets_file,(data)=>{
+          import_widgets = "\n"+import_widgets+"\n";
+          var retour = helpers.placeInPlaceHolder(data,"// DEBUT-IMPORT-PLACEHOLDER","// FIN-IMPORT-PLACEHOLDER",import_widgets);
 
           art_merge = "\n"+art_merge+"\n";
           var retour = helpers.placeInPlaceHolder(retour,"// DEBUT-WIDGET-ARTICLE-PLACEHOLDER","// FIN-WIDGET-ARTICLE-PLACEHOLDER",art_merge);
@@ -156,7 +171,7 @@ module.exports = {
           all_meta_merge = "\n"+all_meta_merge+"\n";
           var retour = helpers.placeInPlaceHolder(retour,"// DEBUT-WIDGET-ALL-META-PLACEHOLDER","// FIN-WIDGET-ALL-META-PLACEHOLDER",all_meta_merge);
 
-          helpers.createFile(modules_base_dir,env_file,retour);
+          helpers.createFile(modules_base_dir,widgets_file,retour);
         });
       });
     });
