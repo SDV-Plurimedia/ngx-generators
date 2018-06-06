4913c3ae13814aedb6d9552494855a7d16ab1e93
Alexandre SPAHN <alexandre@sdv.fr>
2018-06-06 11:38:46 +0200

    UPD - Ajout actions de masse

--- generator/templates/modules/modules.displayrules.ts
+++ generator/templates/modules/modules.displayrules.ts
@@ -1,20 +1,17 @@
-import {Injectable} from '@angular/core';
-// DEBUT IMPORT PLACEHOLDER
+import { Injectable } from '@angular/core';
 
+// DEBUT IMPORT PLACEHOLDER
 // FIN IMPORT PLACEHOLDER
 
 // ce fichier s'occupe de charger les display rules de tous les modules
 @Injectable()
 export class DisplayRules {
+  public displayRulesServices: Array<Object> = [];
 
-    public displayRulesServices: Array<Object> = [];
-
-  constructor(
-    // RULES INCLUES AUTOMATIQUEMENT
-    // FIN RULES INCLUES AUTOMATIQUEMENT
-  ) {
+  constructor() // RULES INCLUES AUTOMATIQUEMENT
+  // FIN RULES INCLUES AUTOMATIQUEMENT
+  {
     // PUSH DES RULES INCLUES AUTOMATIQUEMENT
     // FIN PUSH DES RULES INCLUES AUTOMATIQUEMENT
   }
-
-};
+}
