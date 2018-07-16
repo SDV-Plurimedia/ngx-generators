3a232bafaa93a143a48d8e4b27f8cc533469d0ad
Sébastien Chatout <s.chatout@sdv.fr>
2018-07-16 11:50:35 +0200

    Retour de l'accolade disparue

--- generator/main.js
+++ generator/main.js
@@ -8,42 +8,40 @@ process.title = 'ng2-generators';
 //Generateur de script ts (à lancer avec node > 5)
 const helpers = require('../helpers');
 const fs = require('fs');
-const installPath = __dirname+"/generators/";
+const installPath = __dirname + '/generators/';
 
 //lister les arguments
 /*process.argv.forEach(function (val, index, array) {
   console.log(index + ': ' + val);
 });*/
 helpers.printSeparator();
-console.log(" GENERATEUR DE CODE ANGULAR ");
+console.log(' GENERATEUR DE CODE ANGULAR ');
 
-if(process.argv.length == 2){
+if (process.argv.length == 2) {
   helpers.printSeparator();
   //Afficher la liste des générateurs existant
-  console.log("Liste des générateurs dispo:");
+  console.log('Liste des générateurs dispo:');
   fs.readdir(installPath, function(err, items) {
-      for (var i=0; i<items.length; i++) {
-          console.log(' - '+items[i].split('.').shift());
-      console.log(' ');
-      helpers.askData('Que voulez vous générer? ',generateType);
+    for (var i = 0; i < items.length; i++) {
+      console.log(' - ' + items[i].split('.').shift());
+    }
+    console.log(' ');
+    helpers.askData('Que voulez vous générer? ', generateType);
   });
-}
-else{
+} else {
   generateType(process.argv[2]);
 }
 
-function generateType(type){
+function generateType(type) {
   helpers.printSeparator();
   //verifie l'existence du fichier
-  try{
-    fs.accessSync(installPath+type+".js");
-    var generator = require("./generators/"+type);
+  try {
+    fs.accessSync(installPath + type + '.js');
+    var generator = require('./generators/' + type);
     //console.log(generator);
     generator.generate(process.argv);
-  }
-  catch(e){
-    console.log(" le generateur de "+type+" n'existe pas ");
+  } catch (e) {
+    console.log(' le generateur de ' + type + " n'existe pas ");
     console.log(e.stack);
   }
-
 }
